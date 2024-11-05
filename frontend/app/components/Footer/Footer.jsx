cde179b5e7ff1180f19c2ddb0b121d0a3943553b
shigenogoro <kyle990987@gmail.com>
2024-11-05 13:24:53 -0500

    Create Navbar and Footer Skeleton

--- frontend/app/components/Footer/Footer.jsx
+++ frontend/app/components/Footer/Footer.jsx
@@ -4,28 +4,28 @@ const Footer = () => {
     return (
         <div id="footer-container">
             <div id="footer-left">
-                <h3 className='footer-title'>Explore</h3>
+                <h3 id='footer-left-title'>Explore</h3>
                 <span>Latest Recipes</span>
                 <span>Popular Recipes</span>
             </div>
             <div id="footer-right">
-                <h3 className='footer-title'>Contach with Us</h3>
+                <h3 id='footer-right-title'>Contach with Us</h3>
                 {/* Instagram Page */}
                 <div id="social-container">
                     <a 
-                        href="https://www.instagram.com/kyle990987/"
+                        href="#"
                         className='fa fa-instagram social-icon'
                     ></a>
 
                     {/* Facebook Page */}
                     <a 
-                        href="https://www.facebook.com/kyle.wen.12"
+                        href="#"
                         className='fa fa-facebook social-icon'  
                     ></a>
 
                     {/* Twitter Page */}
                     <a 
-                        href="https://www.facebook.com/kyle.wen.12"
+                        href="#"
                         className='fa fa-twitter social-icon'  
                     ></a>
                 </div> 
--- frontend/app/components/Navbar/Navbar.jsx
+++ frontend/app/components/Navbar/Navbar.jsx
@@ -1,14 +1,16 @@
+import './Navbar.css'
+
 const Navbar = () => {
     return (
-        <nav>
-            <div>
-                <span>Recipe App</span>
+        <nav id="navbar">
+            <div id="nav-left-container">
+                <h1 id="nav-title">Recipe App</h1>
             </div>
-            <div>
-                <ul>
-                    <li><a>Upload Recipe</a></li>
-                    <li><a>Recipes</a></li>
-                    <li><a>Logout</a></li>
+            <div id="nav-right-container">
+                <ul id='nav-list'>
+                    <li className='nav-item'><a>Upload Recipe</a></li>
+                    <li className='nav-item'><a>Recipes</a></li>
+                    <li className='nav-item'><a>Logout</a></li>
                 </ul>
             </div>
         </nav>
