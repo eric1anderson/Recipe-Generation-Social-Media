9516de86c3d56fac9197555ab6aa946ba0e3dd40
ibizabroker <tarun.gowda2000@gmail.com>
2024-11-20 16:54:43 -0500

    shift login to /

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ from routers import shoppinglist, auth, llm_recipes, social_media
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-# app.add_middleware(SessionMiddleware, secret_key='your_secret_key')  # Replace with a secure secret key
+app.add_middleware(SessionMiddleware, secret_key='your_secret_key')  # Replace with a secure secret key
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
