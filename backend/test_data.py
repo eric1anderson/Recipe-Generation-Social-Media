315cee7a4c397106ed9c49bf55a2cc4f9313a2dc
eric1anderson <eric.1anderson00@gmail.com>
2024-11-20 17:04:56 -0500

    added admin to test_data.py

--- backend/test_data.py
+++ backend/test_data.py
@@ -7,21 +7,31 @@ import uuid
 def generate_uuid():
     return str(uuid.uuid4())
 
-def add_test_data(email, password):
+def add_test_data(email, password, is_admin = False):
     engine = create_engine('sqlite:///mydatabase.db')
     Base.metadata.bind = engine
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
 
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    if is_admin:
+        user = User(
+            UserID=generate_uuid(),
+            Email=email,
+            Password=hashed_password.decode('utf-8'),
+            Role=False,
+            Name='Test User'
+        )
 
-    user = User(
-        UserID=generate_uuid(),
-        Email=email,
-        Password=hashed_password.decode('utf-8'),
-        Role=True,
-        Name='Test User'
-    )
+    else:
+
+        user = User(
+            UserID=generate_uuid(),
+            Email=email,
+            Password=hashed_password.decode('utf-8'),
+            Role=True,
+            Name='Test User'
+        )
     session.add(user)
     session.commit()
     print(f"User '{email}' added.")
@@ -91,3 +101,4 @@ def add_test_data(email, password):
 if __name__ == '__main__':
     add_test_data('testuser@example.com', 'password123')
     add_test_data('testuser2@example.com', 'password1234')
+    add_test_data('admin@example.com', 'root', is_admin=True)
